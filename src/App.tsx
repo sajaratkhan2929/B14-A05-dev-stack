a71bbfb60eb3a085aa3f98b3c964e7f480727d7c
sajaratkhan2929 <sajaratkhan29@gmail.com>
2026-09-13 11:56:41 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-pink-500">
+        Dev Stack Builder 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
